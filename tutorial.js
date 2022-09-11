8d9c7fcf57c50f8f72750ab34f88d3853db5fac4
Aleksey <alekseykasyakov@gmail.com>
2022-09-11 13:13:01 -0700

    example export objects short export

--- tutorial.js
+++ tutorial.js
@@ -5,6 +5,4 @@ class SomeMathObject{
         console.log('Object created');
     }
 }
-module.exports.sum = sum;
-module.exports.PI = PI;
-module.exports.SomeMathObject = SomeMathObject; 
+module.exports = {sum : sum, PI : PI, SomeMathObject: SomeMathObject}
